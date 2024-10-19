903233479b5592c01a32df193f4e67973dafbbb4
Volha Sakalouskaya <falcoooneee@proton.me>
2024-10-19 12:42:54 +0000

    test: take user's input

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+MAIN_PROGRAM() {
+  echo -e "\nEnter your username:"
+  read USER_NAME
+
+  if [[ -z $USER_NAME ]]
+  then 
+    # if no such user it adds to database
+    USER_NAME=$(echo $($PSQL ""))
+  fi
+}
+
+
+
+MAIN_PROGRAM
